fef308c2a738e2635dd23df9df656eaf34cfce57
SeptianAgusNurfajri <1904030042@students.unis.ac.id>
2022-01-09 13:15:37 +0700

    tugas pertemuan 7

---
+++ pertemuan7/wpsmt5.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2022 at 06:14 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wpsmt5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calon_mhs`
+--
+
+CREATE TABLE `calon_mhs` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL,
+  `agama` varchar(9) NOT NULL,
+  `sekolah_asal` varchar(20) NOT NULL,
+  `foto` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `calon_mhs`
+--
+
+INSERT INTO `calon_mhs` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto`) VALUES
+(1, 'maulana', 'cikiupa', 'laki-laki', 'islam', 'SMK mandiri', 'foto1.png'),
+(2, 'fikri', 'sukamulya', 'laki-laki', 'islam', 'yupentek', 'foto2.png'),
+(3, 'udin', 'cikupa', 'laki-laki', 'islam', 'smk 1', 'foto3.png'),
+(4, 'rani', 'tigaraksa', 'perempuan', 'islam', 'smk 2', 'foto4.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
